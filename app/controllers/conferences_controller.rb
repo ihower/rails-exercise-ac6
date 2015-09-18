df8d5f90d7be8bf14a9dd264f7ed4610f58d82b8
Wen-Tien Chang <ihower@gmail.com>
2015-09-18 11:27:46 +0800

    CRUD events

---
+++ app/controllers/conferences_controller.rb
@@ -0,0 +1,51 @@
+class ConferencesController < ApplicationController
+
+  def index
+    @events = Event.all
+
+    if params[:event_id]
+      @event = Event.find( params[:event_id] )
+    else
+      @event = Event.new
+    end
+
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def create
+    @event = Event.create( event_params )
+    if @event.save
+      redirect_to conferences_path
+    else
+      @events = Event.all
+      render "index" # use index.html.erb template
+    end
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to conferences_path
+    else
+      @events = Event.all
+      render "index"
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+
+    redirect_to :back
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :url)
+  end
+
+end
