9881d13b6e9fecdd14da9aff16d8ac076702d682
Wen-Tien Chang <ihower@gmail.com>
2015-09-30 10:50:35 +0800

    Add dependent

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   validates_presence_of :name
 
-  has_many :attendees
+  has_many :attendees, :dependent => :destroy
 
   def to_param
     "#{self.id}-#{self.name}"
