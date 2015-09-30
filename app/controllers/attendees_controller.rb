5b21c6c40a0f7fcba879f13734a346282e4ba400
Wen-Tien Chang <ihower@gmail.com>
2015-09-30 10:47:49 +0800

    Add attendee create and index actions

---
+++ app/controllers/attendees_controller.rb
@@ -0,0 +1,27 @@
+class AttendeesController < ApplicationController
+
+  before_action :find_event
+
+  def index
+    @attendees = @event.attendees
+  end
+
+  def create
+    @attendee = @event.attendees.build( attendee_params )
+    if @attendee.save
+      redirect_to conference_attendees_path(@event)
+    else
+      render "index"
+    end
+  end
+
+  protected
+
+  def attendee_params
+    params.require(:attendee).permit(:name)
+  end
+
+  def find_event
+    @event = Event.find( params[:conference_id] )
+  end
+end
