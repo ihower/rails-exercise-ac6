2549fd42bd336d4182bbf8973015552101c2f992
Wen-Tien Chang <ihower@gmail.com>
2015-10-07 10:17:26 +0800

    jquery todo list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get "/jquery" => "welcome#jquery"
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :conferences do
     resources :attendees
