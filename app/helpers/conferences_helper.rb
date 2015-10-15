bfa75909b93170a0c17a6bf0f619ae23b66941d7
Wen-Tien Chang <ihower@gmail.com>
2015-10-15 10:58:47 +0800

    Add view and helper examples

--- app/helpers/conferences_helper.rb
+++ app/helpers/conferences_helper.rb
@@ -1,2 +1,11 @@
 module ConferencesHelper
+
+  def event_link(event)
+    content_tag(:div,
+      link_to(event.name, conference_path(event)) +
+      tag(:br) + 
+      event.description,
+    :class => "user")
+  end
+
 end
