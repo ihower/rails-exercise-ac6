ad897a28e9f830ce820460ffdb3dc820d9dcb16e
Wen-Tien Chang <ihower@gmail.com>
2015-10-21 11:58:58 +0800

    Add facebook API login

--- Gemfile
+++ Gemfile
@@ -9,7 +9,6 @@ gem 'devise'
 gem 'omniauth-facebook'
 
 gem 'rest-client'
-
 gem "paperclip"
 
 gem 'ransack'
@@ -56,7 +55,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'rspec-rails'
-  
+
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,17 @@ class User < ActiveRecord::Base
 
   before_create :generate_authentication_token
 
+  def self.get_fb_data(access_token)
+    res = RestClient.get "https://graph.facebook.com/v2.4/me",  { :params => { :access_token => access_token } }
+
+    if res.code == 200
+      JSON.parse( res.to_str )
+    else
+      Rails.logger.warn(res.body)
+      nil
+    end
+  end
+
   def self.fb_email(fb_token)
     require 'open-uri'
     raw_data = URI.parse("https://graph.facebook.com/v2.4/me?fields=email&access_token=#{fb_token}").read
